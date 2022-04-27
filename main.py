ef47f4d8451422c2c31770fb1ad65244efabaf13
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-04-27 19:54:56 +0200

    Basic converter from plain text to morse

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from morse_loader import MorseLoader
 
 
-morse_loader = MorseLoader()
-print(morse_loader.data)
+morse_table = MorseLoader().data
+
+plain_text = input('Enter the plain text: ').upper()
+
+morse_code = ""
+for char in plain_text:
+    morse_code = ' '.join([morse_code, morse_table[char]])
+
+print(f'Morse code: {morse_code}')
