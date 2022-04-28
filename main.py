df2dc97e7abb30724aa2970706deb0d0df34303d
Zoárd Ehmann <zoard.ehmann@gmail.com>
2022-04-28 18:59:47 +0200

    Implement morse encode & decode functions

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from morse_loader import MorseLoader
+from morse_converter import MorseConverter
 
 
-morse_table = MorseLoader().data
+mc = MorseConverter()
 
-plain_text = input('Enter the plain text: ').upper()
 
-morse_code = ""
-for char in plain_text:
-    morse_code = ' '.join([morse_code, morse_table[char]])
+def show_info():
+    print('\n***********************************************************************************************************************')
+    print('')
+    print('Plain Text - case insensitive, allowed characters: latin letters, numbers, punctuation characters, whitespaces')
+    print('Morse Code - space delimited (between letters), allowed characters: dot (.) = dih, dash (-) = dah, slash (/) = space')
+    print('')
+    print('***********************************************************************************************************************\n')
 
-print(f'Morse code: {morse_code}')
+
+show_info()
+
+while True:
+    route = input(f'Select action:\n0: Help\n1: Plain Text -> Morse Code\n2: Morse Code -> Plain Text\n9: Stop\nChoice: ')
+
+    if route == '0':
+        show_info()
+        continue
+    elif route == '1':
+        result = mc.morse_encode(input('Plain Text: '))
+    elif route == '2':
+        result = mc.morse_decode(input('Morse Code: '))
+    elif route == '9':
+        break
+    else:
+        print('\n!!! Invalid action !!!\n')
+        continue
+
+    print(f'\nResult: {result}\n')
---
+++ morse_converter.py
@@ -0,0 +1,32 @@
+from morse_loader import MorseLoader
+
+
+class MorseConverter():
+
+    def __init__(self):
+        self.morse_table = MorseLoader().data
+
+    def morse_encode(self, inp):
+        morse_code = ""
+
+        for char in inp.upper():
+            if char.isspace():
+                morse_code += '/'
+            else:
+                morse_code += self.morse_table[char]
+            morse_code += ' '
+
+        return morse_code
+
+    def morse_decode(self, inp):
+        plain_text = ""
+
+        for char in inp.split():
+            if char == '/':
+                plain_text += ' '
+            else:
+                for letter, code in self.morse_table.items():
+                    if code == char:
+                        plain_text += letter
+        
+        return plain_text
